2650d9148e266924add344971685a1554b950247
David Caldwell <djcald@cs.washington.edu>
2017-11-04 15:46:54 -0700

    playing around with environment setup

--- environment_info.py
+++ environment_info.py
@@ -1,2 +1,2 @@
-data_dir = 'C:\\Users\\djcald.CSENETID\\SharedCode\\seizurePrediction_data'
+data_dir = 'C:\Users\djcald.CSENETID\SharedCode\seizurePrediction_data'
 scripts = 'C:\Users\djcald.CSENETID\SharedCode\seizurePrediction\scripts'
