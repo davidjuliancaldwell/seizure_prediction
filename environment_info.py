9c150696cfb39416fdfdc0d49714a8cd599eac40
David Caldwell <djcald@cs.washington.edu>
2017-11-04 16:04:35 -0700

    move data directory under master folder

--- environment_info.py
+++ environment_info.py
@@ -1,2 +1,8 @@
-data_dir = 'C:\Users\djcald.CSENETID\SharedCode\seizurePrediction_data'
-scripts = 'C:\Users\djcald.CSENETID\SharedCode\seizurePrediction\scripts'
+import os
+script_dir = os.path.dirname(__file__)
+data_dir = os.path.join(script_dir,'seizurePrediction_data')
+
+                        
+
+#data_dir = 'C:\\Users\\djcald.CSENETID\\SharedCode\\seizurePrediction_data'
+#scripts = 'C:\\Users\\djcald.CSENETID\\SharedCode\\seizurePrediction\\scripts'
