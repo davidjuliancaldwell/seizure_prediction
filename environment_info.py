719d55274316b8a5ca9d414ca863b4dcb3877366
David Caldwell <djcald@cs.washington.edu>
2017-11-04 17:51:34 -0700

    figuring out environment

--- environment_info.py
+++ environment_info.py
@@ -1,6 +1,7 @@
 import os
-script_dir = os.path.dirname(__file__)
+root_dir = os.path.dirname(__file__)
 data_dir = os.path.join(script_dir,'seizurePrediction_data')
+scripts_dir = os.path.join(script_dir,'
 
                         
 
