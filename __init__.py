aeadd6770dafa480eda5d5dc67d638a4f97c3807
David Caldwell <djcald@cs.washington.edu>
2017-11-05 23:44:25 -0800

    reorganize files, start organizing data in notebook

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#from environment_info import data_dir
+#from environment_info import root_dir
+#from environment_info import scripts_dir
+
+from environment_info import *
+from patient_info import *
--- environment_info.py
+++ environment_info.py
@@ -1,9 +1,4 @@
 import os
 root_dir = os.path.dirname(__file__)
-data_dir = os.path.join(script_dir,'seizurePrediction_data')
-scripts_dir = os.path.join(script_dir,'
-
-                        
-
-#data_dir = 'C:\\Users\\djcald.CSENETID\\SharedCode\\seizurePrediction_data'
-#scripts = 'C:\\Users\\djcald.CSENETID\\SharedCode\\seizurePrediction\\scripts'
+data_dir = os.path.join(root_dir,'seizurePrediction_data')
+scripts_dir = os.path.join(root_dir,'scripts')
